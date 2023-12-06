715e5bf51063ece9bda3c03d8ef629195615a6bb
Pedro Bonfa <pedro.bonfa@estudante.ufjf.br>
2023-12-06 20:17:36 -0300

    adiciona função e objeto em estoque.js

---
+++ estoque.js
@@ -0,0 +1,19 @@
+
+let estoque = 
+{
+    joao: 
+    [{tipo:"maca", qtd: 1},
+        {tipo:"pera", qtd: 1},
+    ],
+    maria: 
+    [
+        {tipo:"maca", qtd: 2},{tipo:"banana", qtd: 4},
+    ],
+};
+
+estoque.joao[0].tipo
+
+export function getEstoque()
+{
+    return structuredClone(estoque);
+}
