005dadb50d06127be2d82f026e37c0cd07262563
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-12-13 19:42:42 -0300

    adiciona origem e destino na main

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ function leFormulario(event)
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
     const quantidade = document.entrada.quantidade.value;
+    const origem = document.entrada.quantidade.value;
+    const destino = document.entrada.quantidade.value;
 
-    console.log(`fruta: ${fruta} qtd:  ${quantidade}`);
+
+    console.log(`Solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
