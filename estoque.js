f1340e00ecce1d4b84d5a7c5482029d144369078
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2023-12-13 20:57:56 -0300

    simplifica transação com duas funções

--- estoque.js
+++ estoque.js
@@ -20,8 +20,73 @@ export function getEstoque()
 
 export function transacao(origem, destino, quantidade, fruta)
 {
+    if (origem ===destino)
+    {
+        return;
+    }
     if(origem === 'pomar')
     {
-        estoque[destino][0].qtd += quantidade;
+        dePomarParaPessoa(destino,quantidade,fruta);
+    }
+
+    if(destino === 'pomar')
+    {
+        const pessoa = estoque(origem);
+        let monte;
+        for(let i =0; i<pessoa.length;i++)
+        {
+            if(pessoa[i].tipo === fruta)
+            {
+                monte = pessoa[i];
+                break;
+            }
+            if(!monte)
+            {
+               return;
+            }
+            monte.qtd -= Math.min(quantidade, monte.qtd);
+        }
+    }
+
+    function dePomarParaPessoa(destino, quntidade, fruta)
+    {
+        const pessoa = estoque[destino];
+        let monte;
+        for(let i =0; i<pessoa.length;i++)
+        {
+            if(pessoa[i].tipo === fruta)
+            {
+                monte = pessoa[i];
+                break;
+            }
+            if(!monte)
+            {
+                monte = {'tipo': fruta, 'qnt':0};
+                pessoa.push(monte);
+            }
+            monte.qtd += quantidade;
+            return;
+        }
+        
+    }
+
+    function dePessoaParaPomar(origem, quntidade, fruta)
+    {
+        const pessoa = estoque[destino];
+        let monte;
+        for(let i =0; i<pessoa.length;i++)
+        {
+            if(pessoa[i].tipo === fruta)
+            {
+                monte = pessoa[i];
+                break;
+            }
+            if(!monte)
+            {
+               return;
+            }
+            monte.qtd -= Math.min(quantidade, monte.qtd);
+        }
+        
     }
 }
